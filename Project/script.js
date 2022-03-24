a69587147d79a94531e8456d2f0b36471a2800ed
Dawewed <biryuckov.david@yandex.ru>
2022-03-24 18:06:27 +0300

    3

---
+++ Project/script.js
@@ -0,0 +1,17 @@
+//alert("Hello JS")
+//var a = prompt("Введите число");
+//alert (a);
+function guess() {
+    var a = prompt("Введите число");
+    if (a > 5) {
+        alert("Число должно быть меньше");
+        return guess();
+    } else if (a == 5) {
+        return alert("Угадали");
+    } else {
+        alert("Число должно быть больше");
+        return guess();
+    }
+}
+
+guess();
