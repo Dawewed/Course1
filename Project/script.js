d5df75a2b9b7deda93d69ac5126f021c15a6bf48
Dawewed <biryuckov.david@yandex.ru>
2022-03-25 17:30:54 +0300

    5

--- Project/script.js
+++ Project/script.js
@@ -14,4 +14,28 @@ function guess() {
     }
 }
 
-guess();
+//guess();
+
+
+function timer() {
+    alert("Ты здесь слишком долго");
+}
+//setTimeout(timer, 3000);
+//timer = 1000 (10 секунд)
+function valid() {
+    var name = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+
+    var reg_name = /^[а-яa-zё]+$/i;
+    
+    var reg_password = /^[a-z0-9]{5,100}/i;
+
+    if (reg_name.test(name) == false) {
+        alert("Ошибка. Имя введено неправильно");
+    }
+    if (reg_password.test(password) == false){
+        alert("Ошибка. Пароль введён неправильно");
+    }
+}
+
+document.querySelector(".button").addEventListener("click", valid);
